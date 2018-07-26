6d3f98773c002c377c4d5d8976b88024ef4db86d
hlly <vanhop3499@gmail.com>
2018-07-26 15:25:54 +0700

    macro

---
+++ macro.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+#define IO ios::sync_with_stdio(false); cin.tie(0);
+#define all(v) (v).begin(), (v).end()
+#define fi first
+#define se second
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<ll, int> li;
+const int INF = (int) 1e9;
+const ll LINF = (ll) 1e18;
+int main() {
+    IO;
+    #ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt", "w", stdout);
+	#endif
+    
+    return 0;
+}
